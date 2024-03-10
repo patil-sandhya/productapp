125b6ed07b01cc7b6c3e6b02375f3987d2fe2c60
PatilSandhya <begadesandhya@gmail.com>
2024-03-10 16:09:28 +0530

    register updated

--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -12,21 +12,29 @@ userRouter.get("/",auth, (req,res)=>{
   res.send({msg:"user data..."})
 })
 
-  userRouter.post("/register",  (req, res) => {
-    const {name,email,mobile,passoword,address} =req.body
+  userRouter.post("/register",  async(req, res) => {
+    const {name,email,mobile,password,address} =req.body
+    console.log(req.body)
     try {
-    bcrypt.hash(passoword, 5, async(err,hash)=>{
-        if(err){
-            res.send(err)
-        }else{
-            const user = new UserModel({
-                name,email,address,mobile,
-                passoword:hash
-            });
-            await user.save();
-            res.status(200).send({ msg: "New user registered", newUser: req.body });
-        }
-    })
+      let userExist = await UserModel.findOne({email})
+      if(userExist){
+        res.status(200).json({msg:"User already exist, please login"})
+      }else{
+        bcrypt.hash(password, 5, async(err,hash)=>{
+          console.log("hash")
+            if(err){
+                res.send(err)
+            }else{
+                const user = new UserModel({
+                    name,email,address,mobile,
+                    password:hash
+                });
+                await user.save();
+                res.status(200).send({ msg: "New user registered", newUser: req.body });
+            }
+        })
+      }
+    
       
     } catch (error) {
       res.status(400).send({"err":error});
@@ -34,19 +42,19 @@ userRouter.get("/",auth, (req,res)=>{
   });
   
 userRouter.post("/login", async(req,res)=>{
-    let {email,passoword}=req.body
+    let {email,password}=req.body
     try {
         const user = await UserModel.findOne({email})
         if(user){
-        bcrypt.compare(passoword, user.passoword, (err, result)=>{
+        bcrypt.compare(password, user.password, (err, result)=>{
             if(err){
                 res.status(400).send({ msg: "please try again later" });
             }else
             if(result == true){
                 const token = jwt.sign({user:user},process.env.secretKey,{expiresIn:3600})
-                res.status(200).send({ msg: "Login success ","token":token });
+                res.status(200).send({ msg: "Login successfull", "token":token, "name":user.name, "address": user.address,"email":user.email, "mobile":user.mobile });
             }else{
-              res.status(400).send({ msg: "please try again later" });
+              res.status(400).send({ msg: "invalid credential" });
             }
 
         })
